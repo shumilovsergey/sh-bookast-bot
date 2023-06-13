31016fd671e4bd5a48a12ac9e1ba96fc6c13761c
shumilov <wumilovsergey@gmail.com>
2023-06-13 16:55:25 +0300

    up

--- app/views.py
+++ app/views.py
@@ -3,14 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+import requests
+from project.const import BOT_TOKEN
 
-#   IMPORT MODELS!
-# from .models import Users
 
-#   EXAMPLE
-# def example(request):
-#     context = ''
-#     return render(request, 'proxy/login.html', {'context':context})
 
 def home(request):
     return render(request, 'app/page_1.html')
@@ -19,3 +16,29 @@ class HelloWorldView(APIView):
     def get(self, request):
         data = {"message": "Hello, world!"}
         return Response(data)
+    
+
+
+
+def webhook(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body.decode('utf-8'))
+        message = data['message']
+        chat_id = message['chat']['id']
+        send_message(chat_id, 'Hello, World!')
+        
+    return HttpResponse()
+
+def send_message(chat_id, text):
+    
+    url = f"https://api.telegram.org/{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": text
+    }
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        print("Message sent successfully!")
+    else:
+        print("Failed to send the message.")
--- project/const.py
+++ project/const.py
@@ -3,3 +3,6 @@ import os
 
 load_dotenv()
 # ALLOWED_HOST = os.getenv("ALLOWED_HOST")
+
+# BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "test"
